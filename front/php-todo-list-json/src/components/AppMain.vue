13bd6ccebac1e815a036176b0e218fe7b4c1ac62
Dennis Empirio <dennis.empirio@gmail.com>
2023-01-12 17:32:31 +0100

    first commit

---
+++ front/php-todo-list-json/src/components/AppMain.vue
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<template>
+    <div class="box">
+        <form method="post">
+            <input type="text" name="todo_item">
+            <input type="submit" value="Add">
+        </form>
+    </div>
+</template>
+
+<style>
+</style>
